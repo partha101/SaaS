ac299da34207d444bd7160a6bdffb0cca9e16943
cs <cs@node33.com>
2015-09-03 14:44:46 -0400

    heap

---
+++ build_max_heap.py
@@ -0,0 +1,15 @@
+def max_heapify(A, i):
+    left = 2 * i + 1
+    right = 2 * i + 2
+    largest = i
+    if left < len(A) and A[left] > A[largest]:
+        largest = left
+    if right < len(A) and A[right] > A[largest]:
+        largest = right
+    if largest != i:
+        A[i], A[largest] = A[largest], A[i]
+        max_heapify(A, largest)
+
+def build_max_heap(A):
+    for i in range(len(A) // 2, -1, -1):
+        max_heapify(A, i)
