5969af2137e0c9f35cb52f962da136c1504b612e
partha101 <thakuriapartha780@gmail.com>
2015-09-04 19:10:41 -0400

    new files

--- heap.py
+++ heap.py
@@ -1,20 +1,20 @@
-from build_max_heap import build_max_heap
-from build_max_heap import max_heapify
+from build_max_heap import build_max_heap 
+from build_max_heap import max_heapify    
 class heap:
 	def __init__(self):
-		self.list=[]
+		self.list=[]             
 	def insert(self,x):
 		self.list.append(x)
-		build_max_heap(self.list)
+		build_max_heap(self.list) 
 	def is_empty(self):
-		return self.list==[]
+		return self.list==[]      
 	def extract_max(self):
 		self.list[len(self.list)-1],self.list[0]=self.list[0],self.list[len(self.list)-1]
 		print self.list.pop()
-		build_max_heap(self.list)
+		build_max_heap(self.list) 
 	def heap_sort(self):
 		while not self.list.is_empty():
-			extract_max()
+			extract_max()       
 	def delete(self):
 		self.list.pop()
 	def delete_item(self,k):
@@ -24,16 +24,16 @@ class heap:
 				break
 		self.list[len(self.list)-1],self.list[index]=self.list[index],self.list[len(self.list)-1]
 		self.list.pop()
-		build_max_heap(self.list)
+		build_max_heap(self.list) 
 	def print_list(self):
 		for i in self.list:
-			print i
+			print i 	   
 	def change_key(self,i,key):
 		if key<self.list[i]:
 			print "no insertion"
 		else :
 			self.list[i]=key
-			build_max_heap(self.list)
+			build_max_heap(self.list)   
 		
 			
 heap1=heap()
@@ -44,12 +44,12 @@ for i in range(x):
 	new.append(input())
 for i in new:
 	heap1.insert(i)
-x=raw_input("enter delete if you want to delete the max element or change if you want to change some key\n")
-if x=="delete":
-	heap1.extract_max()
-elif x=="change":
+name=raw_input("enter delete if you want to delete the max element or change if you want to change some key\n")
+if name=="delete":
+	heap1.extract_max() 
+elif name=="change":
 	i,k=input("enter the index i you want to change with the key k in the form i,k\n")
-	heap1.change_key(i,k)
+	heap1.change_key(i,k) 
 else :
 	print "invalid input"
 	
