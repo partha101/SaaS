0a956140020aecd7865229b01c0cbac9da0a77ac
partha101 <thakuripartha780@gmail.com>
2015-09-08 17:23:57 +0530

    quick sort

--- quick.py
+++ quick.py
@@ -3,18 +3,20 @@ def quick_sort(A,low,high):
 		p=partition(A,low,high)
 		quick_sort(A,low,p-1)
 		quick_sort(A,p+1,high)
-	for i in A:
-		print i
 def partition(A,low,high):
 	pivot = A[high]
-    	i = low #place for swapping
+    	i = low 
     	for j in range(low,high):
         	if A[j] <= pivot:
             		A[i],A[j]=A[j],A[i]
             		i = i + 1
-    	A[i],A[high]=A[high],A[i]
+    	temp=A[i]
+	A[i]=A[high]
+	A[high]=temp
     	return i
 A=[12,23,1,222,43]
 quick_sort(A,0,4)
+for i in A:
+	print i
 
 
